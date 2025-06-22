9f0bc02721fd4587b470b1f9cbf939d113f5b78e
Johanna Liza <johanna.herrera@al.infnet.edu.br>
2025-06-22 17:05:28 -0300

    AT finalizado

--- AT/TurismoApp/Models/Usuario.cs
+++
@@ -1,9 +0,0 @@
-namespace TurismoApp.Models
-{
-    public class Usuario
-    {
-        public string Nome { get; set; } = "";
-        public string Email { get; set; } = "";
-        public string Senha { get; set; } = "";
-    }
-}
